7848b36e1abdaa2b53adbf5dbba1d7fc32250eda
Ngọc Thắng <89723279+thangdevweb@users.noreply.github.com>
2023-09-04 20:28:32 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -8,8 +8,8 @@ const initApiRoutes = require('./routes/apiRoutes')
 const port = process.env.PORT || 6060
 
 app.use(cors())
-app.use(express.json({limit: '10mb'}));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 connectDB()
 
