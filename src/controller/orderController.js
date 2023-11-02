f3b21b7c5a8f926343593b129a505d04abe18fdc
Ngọc Thắng <89723279+thangdevweb@users.noreply.github.com>
2023-11-02 21:09:33 +0700

    Update orderController.js

--- src/controller/orderController.js
+++ src/controller/orderController.js
@@ -15,7 +15,7 @@ const createOrder = async (req, res) => {
     return res.status(200).json(data);
   } catch (e) {
     console.log(e);
-    return res.status(200).json({ errCode: -1, errMessage: 'Server error!' });
+    return res.status(200).json({ errCode: -1, errMessage: 'Server error!', err: e });
   }
 };
 
